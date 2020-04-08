de6b72ee782e7ba9906746b3441dce668b4c39fc
Norberto Santiago <norbertosantiago82@gmail.com>
2020-04-08 19:00:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,6 +17,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
     if @@cart.empty?
+      resp.write "Your cart is empty."
     else
       resp.write "Path Not Found"
     end
